00b442a6cf4b6c836f75391082d6fbe3169b7a41
Fscripter <jpabloangulo2003@gmail.com>
2022-09-03 11:09:39 -0500

    Add gravity and ground detection

--- Models/Player.js
+++ Models/Player.js
@@ -1,55 +1,81 @@
-class Player
-{
-    #radio;
-    #xCentro;
-    #yCentro;
-    #colorRelleno;
-    #colorContorno;
+class Player {
+  constructor() {
+    this.posicion = {
+      x: 0,
+      y: 0,
+    };
+    this.size = {
+      w: 50,
+      h: 100,
+    };
+    this.imgBase = new Image();
+    this.imgBase.src = "./Sprites/Player/base.jpg";
+    this.gravedadIntensidad = 10;
+    this.deltaTime = 60 / 1000; // obtener el tiempo que pasa entre frame y frame
+    this.isGround = false; //Variable para saber si esta tocando el suelo
+    this.collision = {
+      x: false,
+      y: false,
+    };
+  }
 
-
-    constructor (radioIncial, xInicial, yInicial, contorno, relleno)
-    {
-
-        this.#radio = radioIncial;
-        this.#xCentro = xInicial;
-        this.#yCentro = yInicial;
-        this.#colorContorno = contorno;
-        this.#colorRelleno = relleno;
-        this.imgBase = new Image();
-        this.imgBase.src="./Sprites/Player/base.jpg";
-
+  dibujar(ctx, mapArray) {
+    ctx.drawImage(
+      this.imgBase,
+      this.posicion.x,
+      this.posicion.y,
+      this.size.w,
+      this.size.h
+    );
+    this.aplicarGravedad(mapArray);
+  }
+  moverIzquierda() {
+    if (this.posicion.x > 0 && this.collision.x == false) {
+      this.posicion.x += -10;
+    }
+  }
+  moverDerecha() {
+    if (this.collision.x == false) {
+      this.posicion.x += 10;
+    }
+  }
+  aplicarGravedad(mapArray) {
+    if (!this.isGround) {
+      this.posicion.y += this.gravedadIntensidad * this.deltaTime * 10;
+    }
+    if (this.posicion.y > 12 * 50) {
+      this.posicion.y = 0;
+      this.posicion.x = 0;
+    }
+    this.colisionTerreno(mapArray);
+  }
+  colisionTerreno(mapArray) {
+    this.cuadrantePosicion = {
+      x: Math.floor(this.posicion.x / this.size.w),
+      y: Math.ceil(this.posicion.y / 50),
+    };
+    if (
+      mapArray[this.cuadrantePosicion.y][this.cuadrantePosicion.x] == "T" ||
+      mapArray[this.cuadrantePosicion.y][this.cuadrantePosicion.x] == "L" ||
+      mapArray[this.cuadrantePosicion.y][this.cuadrantePosicion.x] == "D"
+    ) {
+      this.isGround = true;
+    } else {
+      this.isGround = false;
     }
 
-    dibujar (ctx)
-    {
-        // ctx.beginPath();
-        ctx.drawImage(this.imgBase,this.#xCentro, this.#yCentro);
-        // ctx.arc(this.#xCentro,this.#yCentro,this.#radio,0, 2.0 * Math.PI);
-        // ctx.fillStyle = this.#colorRelleno;
-        // ctx.fill();
-        // ctx.strokeStyle = this.#colorContorno;
-        // ctx.stroke();
-        // ctx.closePath();c
+    this.collision.x = false;
+    if (
+      mapArray[this.cuadrantePosicion.y - 1][this.cuadrantePosicion.x + 1] ==
+        "T" ||
+      mapArray[this.cuadrantePosicion.y - 1][this.cuadrantePosicion.x + 1] ==
+        "L" ||
+      mapArray[this.cuadrantePosicion.y - 1][this.cuadrantePosicion.x + 1] ==
+        "D" ||
+      mapArray[this.cuadrantePosicion.y - 1][this.cuadrantePosicion.x] == "S"
+    ) {
+      this.collision.x = true;
     }
-
-    moverArriba ()
-    {
-        this.#yCentro += - 10;
-    }
-
-    moverAbajo ()
-    {
-        this.#yCentro += 10;
-    }
-    moverIzquierda ()
-    {
-        this.#xCentro += - 10;
-    }
-
-    moverDerecha ()
-    {
-        this.#xCentro += 10;
-    }
-
-    
+    console.log(this.collision.x);
+  }
 }
--- main.js
+++ main.js
@@ -1,53 +1,35 @@
-window.onload = ()=>{
-    var mapaCanvas = new Mapa();
-    mapaCanvas.cargarZona("Antioquia");
+window.onload = () => {
+  var mapaCanvas = new Mapa();
+  mapaCanvas.cargarZona("Antioquia");
 
-    let request;
-    let player1 = new Player(100, 0, 0, "#ffffff", "#000000");
+  let request;
+  let player1 = new Player(100, 0, 0, "#ffffff", "#000000");
 
-    //Main Loop
-    const performAnimation = () => {
-        request = requestAnimationFrame(performAnimation)
-        //animate something
-        mapaCanvas.limpiar();
-        mapaCanvas.dibujarMapa();
-        player1.dibujar(mapaCanvas.context);
-        
+  //Main Loop
+  const performAnimation = () => {
+    request = requestAnimationFrame(performAnimation);
+    //animate something
+    mapaCanvas.limpiar();
+    mapaCanvas.dibujarMapa();
+    player1.dibujar(mapaCanvas.context, mapaCanvas.mapaArray);
+  };
+  requestAnimationFrame(performAnimation);
+
+  window.addEventListener("keydown", moverPlayer);
+
+  function moverPlayer(e) {
+    switch (e.key) {
+      case "w":
+        player1.moverArriba();
+        break;
+
+      case "a":
+        player1.moverIzquierda();
+        break;
+
+      case "d":
+        player1.moverDerecha();
+        break;
     }
-    requestAnimationFrame(performAnimation);
-
-    
-    
-    window.addEventListener("keydown", moverPlayer);
-
-    function moverPlayer(e)
-    {
-        switch(e.key)
-        {
-            case "w" :
-                player1.moverArriba();
-                break;
-
-            case "a" :
-                player1.moverIzquierda();
-                mapaCanvas.columnaIndice--;
-                break;
-
-            case "d" :
-                player1.moverDerecha();
-                mapaCanvas.columnaIndice++  ;
-                break;
-
-            case "s" :
-                player1.moverAbajo();
-                break;
-
-
-
-        }   
-    }
-
-
-    
-    
-}
+  }
+};
