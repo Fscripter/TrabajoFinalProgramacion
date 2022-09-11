1804479bae1c92e070c5ff59e9c8659bd05b66d7
Fscripter <jpabloangulo2003@gmail.com>
2022-09-11 07:10:42 -0500

    Map fixed and camera suport

--- Models/HUD.js
+++ Models/HUD.js
@@ -61,14 +61,14 @@ class VidaJugador extends Vida {
     this.color = "#00ff00";
   }
   dibujar(ctx, canvasPosition) {
-    ctx.fillRect(-canvasPosition.x + 15, 15, this.width + 10, 30);
+    ctx.fillRect(-canvasPosition.x + 15, -canvasPosition.y + 15, this.width + 10, 30);
     ctx.fillStyle = this.color;
-    ctx.fillRect(-canvasPosition.x + 20, 20, this.percentajeWidth, 20);
+    ctx.fillRect(-canvasPosition.x + 20, -canvasPosition.y + 20, this.percentajeWidth, 20);
     ctx.font = "bold 20px arial";
     ctx.strokeStyle = "#ffffff";
-    ctx.strokeText(this.name, -canvasPosition.x + 15, 70);
+    ctx.strokeText(this.name, -canvasPosition.x + 15, -canvasPosition.y + 70);
     ctx.fillStyle = "#000000";
-    ctx.fillText(this.name, -canvasPosition.x + 15, 70);
+    ctx.fillText(this.name, -canvasPosition.x + 15, -canvasPosition.y + 70);
   }
   calculate() {
     let valorRojo = Math.floor((100 - this.percentajeWidth / 2) * (255 / 100));
