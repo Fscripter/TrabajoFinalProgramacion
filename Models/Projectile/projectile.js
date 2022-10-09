d323511f875667beb0171d8f70914de232e6354c
Fscripter <jpabloangulo2003@gmail.com>
2022-10-09 02:44:53 -0500

    Projectiles add

---
+++ Models/Projectile/projectile.js
@@ -0,0 +1,17 @@
+class Projectile {
+  constructor(position, orientacion, size) {
+    this.positionWorld = position;
+    this.orientacion = orientacion;
+    this.size = size;
+    this.image = new ImagenDerogada("./Sprites/Objects/Cajas1.jpeg");
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.positionWorld.x,
+      this.positionWorld.y,
+      this.size.w,
+      this.size.h
+    );
+  }
+}
