cea56699b2805f40c41456a81ff196a09e3d6172
Muertex24 <muertex47@gmail.com>
2022-10-09 03:23:01 -0500

    bullet add

--- Models/Projectile/bullet/bullet.js
+++ Models/Projectile/bullet/bullet.js
@@ -1,3 +1,19 @@
 class bullet extends Projectile{
-    
+    constructor(position,orientation,damage){
+        this.damage = 30;
+        this.size = {
+            w:10,
+            h:5,
+        },
+        this.image.src = "./bullet.png"
+        this.speed = 30;
+
+    }
+    draw(context) {
+        super.draw(context)
+        this.move()
+    };
+    move(){
+        this.positionWorld.x += this.speed
+    }
 }
