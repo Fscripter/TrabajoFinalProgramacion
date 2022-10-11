8d70c21d8fb84a16040b6aebfe89166d5db04b66
Muertex24 <muertex47@gmail.com>
2022-10-11 02:25:22 -0500

    Laser animation

--- Models/Projectile/laser/laser.js
+++ Models/Projectile/laser/laser.js
@@ -2,21 +2,58 @@ class Laser extends Projectile {
   constructor(posicion, orientation) {
     super(posicion, orientation, {
       w: 100,
-      h: 3,
+      h: 10,
     });
-    this.image = new ImagenDerogada("./Sprites/Balas/Laser.png");
-    this.speed = 15;
+    this.animation = new Animator(
+      {
+        states: ["living"],
+        animations: [
+          {
+            id: "living",
+            transitionTime: 50,
+            loop: false,
+            animaciones: {
+              derecha: [
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/1.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/2.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/3.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/4.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/5.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/6.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Right/7.png"),
+              ],
+              izquierda: [
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/1.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/2.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/3.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/4.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/5.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/6.png"),
+                new ImagenDerogada("./Sprites/Balas/Laser/Left/7.png"),
+              ],
+            },
+          },
+        ],
+      },
+      this.orientation
+    );
+    this.speed = 12;
     if (this.orientation == "L") {
       this.speed *= -1;
     }
     this.sound = new Audio("./Sprites/Player/Sound/laser.mp3");
     this.sound.play();
+    this.damage = 30;
   }
   move() {
     this.positionWorld.x += this.speed;
   }
   draw(context) {
-    super.draw(context);
     this.move();
+    this.image = this.animation.drawAnimation();
+    if (this.animation.lastFrame) {
+      this.delete();
+    }
+    super.draw(context);
   }
 }
