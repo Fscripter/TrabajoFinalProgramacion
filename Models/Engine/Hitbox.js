da417648ace40cadfb20adbbbf0b18d9e35ee439
Fscripter <jpabloangulo2003@gmail.com>
2022-11-09 22:38:22 -0500

    V 1.0.0

--- Models/Engine/Hitbox.js
+++ Models/Engine/Hitbox.js
@@ -1,48 +1,47 @@
 class Hitbox {
-    constructor(gameObject, Character) {
-        this.normal = {
-            x: gameObject.positionWorld.x,
-            y: gameObject.positionWorld.y,
-            w: gameObject.size.w,
-            h: gameObject.size.h,
-        };
-        this.down = {
-            x: gameObject.positionWorld.x,
-            y: gameObject.positionWorld.y,
-            w: gameObject.size.w,
-            h: gameObject.size.h / 1.4,
-        };
+  constructor(gameObject, Character) {
+    this.normal = {
+      x: gameObject.positionWorld.x,
+      y: gameObject.positionWorld.y,
+      w: gameObject.size.w,
+      h: gameObject.size.h,
+    };
+    this.down = {
+      x: gameObject.positionWorld.x,
+      y: gameObject.positionWorld.y,
+      w: gameObject.size.w,
+      h: gameObject.size.h / 1.4,
+    };
+    this.measure = this.normal;
+    this.gameObject = gameObject;
+    this.states = ["Estatico", "Down"];
+    this.currentState = "Estatico";
+  }
+  getPosition() {
+    return this.gameObject.positionWorld;
+  }
+  changeState(newState) {
+    if (newState != this.currentState) {
+      this.currentState = newState;
+      if (newState == "Estatico") {
         this.measure = this.normal;
-        this.gameObject = gameObject;
-        this.states = ["Estatico", "Down"];
-        this.currentState = "Estatico";
-    }
-    getPosition() {
-        return this.gameObject.positionWorld;
-    }
-    changeState(newState) {
-        if (newState != this.currentState) {
-            this.currentState = newState;
-            if (newState == "Estatico") {
-                this.measure = this.normal;
-                this.updateMeasure();
-            }
-            else {
-                this.measure = this.down;
-                this.updateMeasure();
-            }
-        }
-    }
-    updateMeasure() {
-        this.measure.x = this.getPosition().x;
-        this.measure.y = this.getPosition().y;
-        if (this.currentState == "Down") {
-            this.measure.y += this.gameObject.size.h - this.measure.h;
-        }
-    }
-    draw(context) {
         this.updateMeasure();
-        context.strokeStyle = "#00FF00";
-        context.strokeRect(this.measure.x, this.measure.y, this.measure.w, this.measure.h);
+      } else {
+        this.measure = this.down;
+        this.updateMeasure();
+      }
     }
+  }
+  updateMeasure() {
+    this.measure.x = this.getPosition().x;
+    this.measure.y = this.getPosition().y;
+    if (this.currentState == "Down") {
+      this.measure.y += this.gameObject.size.h - this.measure.h;
+    }
+  }
+  draw(context) {
+    this.updateMeasure();
+    // context.strokeStyle = "#00FF00";
+    // context.strokeRect(this.measure.x, this.measure.y, this.measure.w, this.measure.h);
+  }
 }
